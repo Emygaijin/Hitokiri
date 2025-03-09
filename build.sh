ca1e760d1640a88fb4fede9f13e54a375a8bd7be
Emygaijin <106027927+Emygaijin@users.noreply.github.com>
2025-03-09 23:44:56 +0000

    Fixed build.sh script for Render deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-set -0 errexit
+#!/bin/bash
+set -e  # This ensures the script exits immediately if a command fails
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
