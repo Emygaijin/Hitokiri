918a9e555b2fcbcb1929cbe621eaff04753ad2c2
Emygaijin <106027927+Emygaijin@users.noreply.github.com>
2025-03-10 07:54:25 +0000

    Removed build.sh for new deployment

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e  # This ensures the script exits immediately if a command fails
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
-python --version
